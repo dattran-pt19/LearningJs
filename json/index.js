e04a882e7d9c4288de19b6b5a6985d2a422a537a
Trần Đạt <dattran.pt19@gmail.com>
2022-06-08 00:27:30 +0700

    Improve CLI list students

--- json/index.js
+++ json/index.js
@@ -3,33 +3,40 @@ var fs = require('fs');
 var Student = require('./student');
 
 var listGenders = ['Male', 'Female'];
-var listOptions = ['View students', 'Add student'];
+var listOptions = ['View students', 'Add student', 'Save & Exit'];
+
+var listStudentsJson = fs.readFileSync('./list_student.json');
+var listStudents = JSON.parse(listStudentsJson).listStudents;
 
 function showOption() {
 	var option = readlineSync.keyInSelect(listOptions, 'Choose your action you wan\'t? ');
 
-switch (option) {
-	case 0:
-	var listStudentsJson = fs.readFileSync('./list_student.json');
-	var listStudents = JSON.parse(listStudentsJson).listStudents;
-	console.log(listStudents);
-	break;
-	case 1:
-	var listStudentsJson = fs.readFileSync('./list_student.json');
-	var listStudents = JSON.parse(listStudentsJson).listStudents;
-
-	var studentName = readlineSync.question('Enter student\'s name ');
-	var studentGender = readlineSync.keyInSelect(listGenders, 'Choose student\'s gender ');
-	var newStudent = new Student(studentName, listGenders[studentGender]);
-	listStudents.push(newStudent);
-	listStudentsJson = JSON.stringify({listStudents: listStudents});
-	fs.writeFileSync('./list_student.json', listStudentsJson);
-	console.log('Added successfully');
-	showOption();
-	break;
-	default:
-	console.log('Quit');
-};
+	switch (option) {
+		case 0:
+		viewListStudents();
+		break;
+		case 1:
+		var studentName = readlineSync.question('Enter student\'s name ');
+		var studentGender = readlineSync.keyInSelect(listGenders, 'Choose student\'s gender ');
+		var newStudent = new Student(studentName, listGenders[studentGender]);
+		addStudent(newStudent);
+		break;
+		case 2:
+		listStudentsJson = JSON.stringify({listStudents: listStudents});
+		fs.writeFileSync('./list_student.json', listStudentsJson);
+		break;
+	};
 };
 
 showOption();
+
+function viewListStudents() {
+	console.log(listStudents);
+	showOption();
+}
+
+function addStudent(student) {
+	listStudents.push(student);
+	console.log('Added successfully');
+	showOption()
+}
